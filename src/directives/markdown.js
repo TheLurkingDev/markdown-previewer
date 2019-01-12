8a35ae62f1add9dce30288e845426f06f3518200
Darren Cook <thelurkingdev@gmail.com>
2019-01-12 11:59:36 -0500

    Extracted regex to rules array.

--- src/directives/markdown.js
+++ src/directives/markdown.js
@@ -1,9 +1,15 @@
+const rules = [
+    [/^#{1}\s([^\n]+)/, '<h1>$1</h1>']
+];
+
 export default {
     bind(el) {        
         let html = el.textContent;
-        console.log('initial value: ', html)
-        html = html.replace(/^#{1}\s([^\n]+)/, '<h1>$1</h1>');
-        console.log('edited value: ', html);
+        rules.forEach(([rule, template]) => {
+            html = html.replace(rule, template)
+        })
+        
+        
         el.innerHTML = html;
     }
 }
